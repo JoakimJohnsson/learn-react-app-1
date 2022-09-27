4db5a6766cf60f5295f4f4c332bbf7b543b9922f
JoakimJohnsson <joakim.johnsson@su.se>
2022-09-27 22:11:39 +0200

    #16 - Use local storage to hold recipes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import RecipeList from "./components/recipe/RecipeList";
 import {mockData} from "./components/mockData";
-
 
 export const RecipeContext = React.createContext(undefined);
 
 function App() {
-    const [recipes, setRecipes] = useState(mockData);
+    const LOCAL_STORAGE_KEY = "cookingWithReact.recipes";
+    // Set to true to clear local storage before loading recipes.
+    const clearLocalStorage = false;
+
+    // Loading recipes from mock data or local storage.
+    const [recipes, setRecipes] = useState(() => {
+        // Clear local storage to reset app.
+        if (clearLocalStorage) {
+            localStorage.clear();
+        }
+        const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (recipeJSON == null) {
+            return mockData;
+        } else {
+            return JSON.parse(recipeJSON);
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
+    }, [recipes]);
+
     // Adding recipe state to context.
     // Using shorthand: {recipe: recipe, setRecipes: setRecipes} -> {recipes, setRecipes}.
     const recipeContextValue = {
