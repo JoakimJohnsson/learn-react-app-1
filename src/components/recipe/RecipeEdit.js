855c7c2ffc655f77a3a4afac11ca9cedcdeb7f08
JoakimJohnsson <joakim.johnsson@su.se>
2022-09-29 07:59:01 +0200

    #17 - Add edit form inputs (w.i.p)

--- src/components/recipe/RecipeEdit.js
+++ src/components/recipe/RecipeEdit.js
@@ -22,9 +22,47 @@ const RecipeEdit = ({modalId, showModal, setShowModal, recipe}) => {
             </Modal.Header>
 
             <Modal.Body>
-                <p>{recipe.description}</p>
+                <p className={"lead"}>{recipe.description}</p>
+                <form>
+                    <label className={"form-label"} htmlFor={"name"}>Name:</label>
+                    <input className={"form-control"} type={"text"} name={"name"}/>
+                    <label className={"form-label"} htmlFor={"cookingTime"}>Cooking time:</label>
+                    <input className={"form-control"} type={"text"} name={"cookingTime"}/>
+                    <label className={"form-label"} htmlFor={"servings"}>Servings:</label>
+                    <input className={"form-control"} type={"number"} min={"1"} name={"servings"}/>
+                    <label className={"form-label"}>Instructions:</label>
+                    <div className={"row"}>
+                        <div className={"col-12 col-sm-8"}>
+                            Input instruction
+                        </div>
+                        <div className={"col-12 col-sm-4 text-end"}>
+                            <button className={"btn btn-sm btn-outline-danger"}>&times; Delete</button>
+                        </div>
+                    </div>
+                    <div className={"row"}>
+                        <div className={"col-12 text-center"}>
+                            <button className={"btn btn-sm btn-outline-primary"}>+ Add</button>
+                        </div>
+                    </div>
+                    <label className={"form-label"}>Ingredients:</label>
+                    <div className={"row"}>
+                        <div className={"col-12 col-sm-4"}>
+                            Input name
+                        </div>
+                        <div className={"col-12 col-sm-4"}>
+                            Input amount
+                        </div>
+                        <div className={"col-12 col-sm-4 text-end"}>
+                            <button className={"btn btn-sm btn-outline-danger"}>&times; Delete</button>
+                        </div>
+                    </div>
+                    <div className={"row"}>
+                        <div className={"col-12 text-center"}>
+                            <button className={"btn btn-sm btn-outline-primary"}>+ Add</button>
+                        </div>
+                    </div>
+                </form>
             </Modal.Body>
-
             <Modal.Footer>
                 <button className={"btn btn-secondary"} onClick={handleClose}>
                     Close
