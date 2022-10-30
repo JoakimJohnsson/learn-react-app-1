01f8b9a89f5b159d1cd600e915080e2dbe2e96cb
JoakimJohnsson <joakim.johnsson@su.se>
2022-10-30 10:04:41 +0100

    #20 - update ingredients - working

--- src/components/recipe/IngredientEdit.js
+++ src/components/recipe/IngredientEdit.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-const IngredientEdit = ({ingredient}) => {
+const IngredientEdit = ({ingredient, handleIngredientChange}) => {
+
+    const [newIngredient, setNewIngredient] = useState({...ingredient})
+
+    const handleChange = (changes) => {
+        setNewIngredient(() => {
+            return {...ingredient, ...changes}
+        });
+        handleIngredientChange(ingredient.id, {...ingredient, ...changes});
+    }
 
     return (
         <div className={"row"}>
             <div className={"col-12 col-sm-4"}>
-                <input className={"form-control"} type={"text"} value={ingredient.name} onChange={() => console.log("TODO")}/>
+                <input className={"form-control"} type={"text"} value={newIngredient.name} onChange={(e) => handleChange({name: e.target.value})}/>
             </div>
             <div className={"col-12 col-sm-4"}>
-                <input className={"form-control"} type={"text"} value={ingredient.amount} onChange={() => console.log("TODO")}/>
+                <input className={"form-control"} type={"text"} value={newIngredient.amount} onChange={(e) => handleChange({amount: e.target.value})}/>
             </div>
             <div className={"col-12 col-sm-4 text-end"}>
                 <button className={"btn btn-sm btn-outline-danger"}>&times; Delete</button>
--- src/components/recipe/RecipeEdit.js
+++ src/components/recipe/RecipeEdit.js
@@ -15,14 +15,36 @@ const RecipeEdit = ({modalId, showModal, setShowModal, recipe}) => {
     const handleClose = () => setShowModal(false);
 
     // Will update new recipe state when any input changes.
-    const handleChange = (e) => {
-        setNewRecipe({...newRecipe, [e.target.name]: e.target.value});
+    const handleChange = (name, value) => {
+        setNewRecipe({...newRecipe, [name]: value});
     }
 
     // Updates the "old" recipe object with the new one when form is submitted.
     const handleSubmit = () => {
         // Replace old recipe with new recipe.
         handleRecipeSubmit(newRecipe.id, newRecipe, recipes, setRecipes);
+    }
+
+    const handleInstructionChange = (id, newInstruction) => {
+        // Copy old recipes state. Make changes to the new one (mutate it). Then set it.
+        const newRecipes = [...recipes];
+        // Get index of the recipe we want to change.
+        const index = newRecipes.findIndex(r => r.id === id);
+        // Replace recipe.
+        newRecipes[index] = newRecipe;
+        // Set new recipes state.
+        setRecipes(newRecipes);
+    }
+
+    const handleIngredientChange = (id, newIngredient) => {
+        // Copy old ingredients from recipe state.
+        const newIngredients = [...recipe.ingredients];
+        // Get index of the ingredient we want to change.
+        const index = newIngredients.findIndex(i => i.id === id);
+        // Replace ingredient.
+        newIngredients[index] = newIngredient;
+        // Handle the change, so it can be updated on submit.
+        handleChange("ingredients", newIngredients);
     }
 
     return (
@@ -37,11 +59,10 @@ const RecipeEdit = ({modalId, showModal, setShowModal, recipe}) => {
             aria-labelledby="contained-modal-title-vcenter"
         >
             <Modal.Header closeButton>
-                <Modal.Title id={modalId}>{newRecipe.name}</Modal.Title>
+                <Modal.Title id={modalId}>{recipe.name}</Modal.Title>
             </Modal.Header>
 
             <Modal.Body>
-                <p className={"lead"}>{newRecipe.description}</p>
                 <form onSubmit={handleSubmit}>
                     <label className={"form-label"} htmlFor={"name"}>Name:</label>
                     <input className={"form-control mb-3"}
@@ -49,15 +70,14 @@ const RecipeEdit = ({modalId, showModal, setShowModal, recipe}) => {
                            name={"name"}
                            id={"name"}
                            value={newRecipe.name}
-                           onChange={e => handleChange(e)}
+                           onChange={e => handleChange(e.target.name, e.target.value)}
                     />
                     <label className={"form-label"} htmlFor={"description"}>Description:</label>
-                    <input className={"form-control mb-3"}
-                           type={"text"}
+                    <textarea className={"form-control mb-3"}
                            name={"description"}
                            id={"description"}
                            value={newRecipe.description}
-                           onChange={e => handleChange(e)}
+                           onChange={e => handleChange(e.target.name, e.target.value)}
                     />
                     <label className={"form-label"} htmlFor={"cookingTime"}>Cooking time:</label>
                     <input className={"form-control mb-3"}
@@ -65,7 +85,7 @@ const RecipeEdit = ({modalId, showModal, setShowModal, recipe}) => {
                            name={"cookingTime"}
                            id={"cookingTime"}
                            value={newRecipe.cookingTime}
-                           onChange={e => handleChange(e)}
+                           onChange={e => handleChange(e.target.name, e.target.value)}
                     />
                     <label className={"form-label"} htmlFor={"servings"}>Servings:</label>
                     <input className={"form-control mb-3"}
@@ -73,17 +93,21 @@ const RecipeEdit = ({modalId, showModal, setShowModal, recipe}) => {
                            name={"servings"}
                            id={"servings"}
                            value={newRecipe.servings}
-                           onChange={e => handleChange(e)}
+                           onChange={e => handleChange(e.target.name, parseInt(e.target.value) || "")}
                     />
                     <label className={"form-label"}>Instructions:</label>
-                    {newRecipe.instructions.map((instruction, index) => <InstructionEdit key={index} instruction={instruction}/>)}
+                    {newRecipe.instructions.map((instruction, index) => (
+                        <InstructionEdit key={index} instruction={instruction} handleInstructionChange={handleInstructionChange}/>
+                    ))}
                     <div className={"row"}>
                         <div className={"col-12 text-center"}>
                             <button className={"btn btn-sm btn-primary"}>+ Add</button>
                         </div>
                     </div>
                     <label className={"form-label"}>Ingredients:</label>
-                    {newRecipe.ingredients.map((ingredient) => <IngredientEdit key={ingredient.id} ingredient={ingredient}/>)}
+                    {newRecipe.ingredients.map((ingredient) => (
+                        <IngredientEdit key={ingredient.id} ingredient={ingredient} handleIngredientChange={handleIngredientChange}/>
+                    ))}
                     <div className={"row"}>
                         <div className={"col-12 text-center"}>
                             <button className={"btn btn-sm btn-primary"}>+ Add</button>
@@ -92,10 +116,7 @@ const RecipeEdit = ({modalId, showModal, setShowModal, recipe}) => {
                     <div className={"row"}>
                         <div className={"col-12 text-end"}>
                             <button className={"btn btn-secondary me-2"} onClick={handleClose}>
-                                Close
-                            </button>
-                            <button type={"submit"} className={"btn btn-outline-primary"}>
-                                Save changes
+                                Close modal
                             </button>
                         </div>
                     </div>
